3c4d80a37b9513cdadbb0cae8e4d6d5670334780
knapp <emiliodamasco04@gmail.com>
2023-08-01 10:59:37 -0700

    added user, city and place

--- models/city.py
+++ models/city.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel
+# from models.base_model import Model
+from sqlalchemy.orm import relationship
 
 
-class City(BaseModel):
+class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     state_id = ""
     name = ""
+    places = relationship("Place", backref="cities", cascade="delete")
