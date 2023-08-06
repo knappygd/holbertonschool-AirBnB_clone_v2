9f9d8c516b90dea584cef62cca7391070462548b
4mmZ <andresmaxi111014@gmail.com>
2023-08-06 16:09:41 -0700

    more test for console

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,31 +1,49 @@
 #!/usr/bin/python3
-"""Module with console unittests"""
-
-
+"""
+    Tests for the HBNB console
+"""
 import unittest
-import console
-import io
-from contextlib import redirect_stdout
+from unittest.mock import patch
+from io import StringIO
+from console import HBNBCommand
+import os
+from models import storage
 
 
-class TestConsole(unittest.TestCase):
-    """Class that tests console"""
+class TestHBNBCommand(unittest.TestCase):
+    """ Tests for the HBNB console """
 
-    def setUp(self):
-        self.console = console.HBNBCommand()
+    @classmethod
+    def setUpClass(cls):
+        """ Create a console object for each test """
+        cls.hbnb = HBNBCommand()
 
+    @classmethod
+    def tearDownClass(cls):
+        """ Delete console object at the end """
+        del cls.hbnb
 
-    def test_create(self):
-        var = "California"
-        with redirect_stdout(io.StringIO()) as a:
-            self.console.onecmd(f"create State")
-        state_id = a.getvalue()
-        with redirect_stdout(io.StringIO()) as f:
-            self.console.onecmd(f"create State name='{var}'")
-        state_id2 = f.getvalue()
-        with redirect_stdout(io.StringIO()) as d:
-            self.console.onecmd(
-                "create City state_id=\"{}\" name='San_Francisco_is_super_cool'".format(str(state_id2)))
-        city_id = d.getvalue()
-        print(f"h{state_id} ,s{state_id2}, c{city_id}")
-        self.assertEqual(1, 1)
+    def tearDown(self):
+        try:
+            os.remove("file.json")
+        except FileNotFoundError:
+            pass
+
+    @unittest.skipIf(os.getenv("HBNB_TYPE_STORAGE") == "db",
+                     "Test only valid for FileStorage")
+    def test_updatedCreate(self):
+        with patch("sys.stdout", new=StringIO()) as mock_output:
+            self.hbnb.onecmd('create User name="Julian" '
+                             'last_name age=8 lat=37.12')
+            _id = mock_output.getvalue().strip()
+
+        storage.reload()
+
+        for key, value in storage.all().items():
+            self.assertEqual(key, "User." + _id)
+            self.assertTrue(getattr(value, 'created_at', None))
+            self.assertTrue(getattr(value, 'updated_at', None))
+            self.assertEqual(getattr(value, 'name', None), 'Julian')
+            self.assertEqual(getattr(value, 'age', None), 8)
+            self.assertEqual(getattr(value, 'lat', None), 37.12)
+            self.assertTrue('last_name' not in value.__dict__)
