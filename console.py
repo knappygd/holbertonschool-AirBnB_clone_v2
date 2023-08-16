85abff6ef371422006e7a5c498b0d5871df1cd6a
knapp <emiliodamasco04@gmail.com>
2023-08-16 11:55:18 -0700

    fixed 5

--- console.py
+++ console.py
@@ -201,13 +201,13 @@ class HBNBCommand(cmd.Cmd):
             if args not in HBNBCommand.classes:
                 print("** class doesn't exist **")
                 return
-            for k, v in storage.all(HBNBCommand.classes[args]).items():
-                if k.split('.')[0] == args:
-                    print_list.append(str(v))
-        else:
-            for k, v in storage._FileStorage__objects.items():
+            to_print = storage.all(HBNBCommand.classes[args])
+            for k, v in to_print.items():
                 print_list.append(str(v))
-
+        else:
+            to_print = storage.all()
+            for k, v in to_print.items():
+                print_list.append(str(v))
         print(print_list)
 
     def help_all(self):
--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -34,9 +34,9 @@ def number(n):
     return "{} is a number".format(n)
 
 
-@app.route("/number_template/<int:n>", strict_slashes=False)
-def number_template(n):
-    return render_template("5-number.html", n=n)
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def num_temp(n):
+    return render_template('5-number.html', n=n)
 
 
 if __name__ == "__main__":
