c35fa3e4894c121a8b370519985205ab27b61344
knapp <emiliodamasco04@gmail.com>
2023-08-16 06:19:21 -0700

    fixed table

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -3,20 +3,21 @@
 """
 from flask import Flask, render_template
 from models import storage
+from models.state import State
 
 app = Flask(__name__)
 
 
-@app.route("/states_list", strict_slashes=False)
-def show_states():
-    states = storage.all("States")
-    return render_template('7-states_list.html', states=states)
-
-
 @app.teardown_appcontext
-def teardown(e):
+def teardown_storage(e):
     storage.close()
 
 
+@app.route("/states_list", strict_slashes=False)
+def show_states():
+    states = storage.all(State)
+    return render_template('7-states_list.html', states=states)
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=5000)
